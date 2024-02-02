e6ddffaba6668a1f45e6ece6e3c4e9788da41fc2
Kenan Bateman <khbateman@gmail.com>
2024-02-02 14:42:05 -0500

    version number change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='TwitterTools',
     packages=find_packages(include=["TwitterTools"]),
-    version='1.0.2',
+    version='1.0.3',
     description='Tools for crawling interacting with Twitter',
     author='Kenan Bateman',
     install_requires=[],
